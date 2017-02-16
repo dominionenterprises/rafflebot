02994fdbd34b58a7dfc10752a6807b962bbc5f73
asmi92 <asmitagosavi92@gmail.com>
2017-02-16 17:03:45 -0500

    conflict resolved

--- public/index.js
+++ public/index.js
@@ -1,38 +1,38 @@
 // You can require() this file in a CommonJS environment.
-require('./dist/js/flat-ui.js');
-// var app = angular.module("raffl", []); 
-// app.controller("socialRaffle", function($scope,$window) {
+//require('./dist/js/flat-ui.js');
+var app = angular.module("raffl", []); 
+app.controller("socialRaffle", function($scope,$window) {
 
-// $scope.signin = true;
-// $scope.rules = false;
+$scope.signin = true;
+$scope.rules = false;
 
-// $scope.openRules = function(){
-// 	$scope.rules = true;
-//  	$scope.signin = false;
-//  	window.location.href = "rules.html";
-// };
+$scope.openRules = function(){
+	$scope.rules = true;
+ 	$scope.signin = false;
+ 	window.location.href = "rules.html";
+};
 
-// $scope.openTwitter = function(){
-// 	$scope.signin = false;
-//  //$window.open('//twitter.com');
-// };
+$scope.openTwitter = function(){
+	$scope.signin = false;
+ //$window.open('//twitter.com');
+};
 
-// $scope.openFacebook = function(){
-// 	console.log("sjhsw");
-// 	$scope.signin = false;
-// // $window.open('//facebook.com');
-// };
-// $scope.openInstagram = function(){
-// 	$scope.signin = false;
-// 	console.log("sjhsw");
-// // $window.open('//instagram.com');
-// };
-// $scope.openGoogle = function(){
-// 	$scope.signin = false;
-// 	console.log("sjhsw");
-// // $window.open('//google.com');
-// };
+$scope.openFacebook = function(){
+	console.log("sjhsw");
+	$scope.signin = false;
+// $window.open('//facebook.com');
+};
+$scope.openInstagram = function(){
+	$scope.signin = false;
+	console.log("sjhsw");
+// $window.open('//instagram.com');
+};
+$scope.openGoogle = function(){
+	$scope.signin = false;
+	console.log("sjhsw");
+// $window.open('//google.com');
+};
 
-// 	});
+	});
 
     
