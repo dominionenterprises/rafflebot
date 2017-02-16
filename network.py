367e4b4666d06308aa817bb626dd4d09f6843cfc
david <bittneradave@gmail.com>
2017-02-16 13:51:24 -0500

    Moved files into public

--- network.py
+++ network.py
@@ -1,19 +1,24 @@
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+import SimpleHTTPServer
+from BaseHTTPServer import HTTPServer
 
 PORT_NUMBER = 8080
 
 #This class will handles any incoming request from
 #the browser
-class myHandler(BaseHTTPRequestHandler):
+class myHandler(SimpleHTTPRequestHandler):
+    #Handler for the GET requests
+    def do_GET(self):
+        if self.path != "/getRaffles":
+            self.path = '/public/'+ self.path
+            return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET( self )
 
-	#Handler for the GET requests
-	def do_GET(self):
-		self.send_response(200)
-		self.send_header('Content-type','text/html')
-		self.end_headers()
-		# Send the html message
-		self.wfile.write("Hello World!")
-		return
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        # Send the html message
+        self.wfile.write("Hello World!")
+        return
 
     def do_POST(self):
         content_len = int(self.headers.getheader('content-length', 0))
